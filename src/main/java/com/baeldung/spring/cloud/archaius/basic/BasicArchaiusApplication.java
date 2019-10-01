a3ab7b207824a49893bdad5cf341dd435775a81a
Msarangi <50727426+Msarangi@users.noreply.github.com>
2019-10-01 20:00:41 +0530

    Update BasicArchaiusApplication.java

--- src/main/java/com/baeldung/spring/cloud/archaius/basic/BasicArchaiusApplication.java
+++ src/main/java/com/baeldung/spring/cloud/archaius/basic/BasicArchaiusApplication.java
@@ -3,16 +3,17 @@ package com.baeldung.spring.cloud.archaius.basic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class BasicArchaiusApplication extends ServletInitializer{
+public class BasicArchaiusApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(BasicArchaiusApplication.class, args);
-	}
-	@Override
+    public static void main(String[] args) {
+        SpringApplication.run(BasicArchaiusApplication.class, args);
+    }
+    @Override
 	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 	      return builder.sources(BasicArchaiusApplication.class);
 	  }
+
 }
